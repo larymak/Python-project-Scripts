faf17c74f543e960555167dd69936f42f153577e
Gagan prajapati <gagan.17.gp@gmail.com>
2021-05-28 18:29:33 +0530

    Update password.py
    Added comments ,So that beginner level python developer could understand the basic functionality of functions imported by random module.

--- RandomPassword/password.py
+++ RandomPassword/password.py
@@ -1,7 +1,7 @@
-import string
-import random
+import string #String module will import all the nessary ascii character
+import random #random module help us to import functions needed to generate random element.
 
-passwrd = string.ascii_letters+string.digits+string.punctuation
+passwrd = string.ascii_letters+string.digits+string.punctuation #This will generate a string consist of all ascii character.
 
 numPass = int(input("How many passwords do you need to be generated? "))
 length = int(input("Enter the length of the password(s): "))
@@ -11,5 +11,5 @@ print("List(s) of Generated passwords: ")
 for pwd in range(numPass):
     pw=''
     for c in range(length):
-        pw += random.choice(passwrd)
+        pw += random.choice(passwrd) # Choice() function returns a random element from a iterable element.
     print(pw)
