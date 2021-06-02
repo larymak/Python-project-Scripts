6ab15f0713192d089566f912b4a29108489d366d
Amit Kulkarni <>
2021-06-02 17:56:50 -0500

    Use random instead of randint

--- RandomNameGen/user_generator.py
+++ RandomNameGen/user_generator.py
@@ -1,20 +1,12 @@
 import requests
-from random import randint
+import random
 
 url = 'https://svnweb.freebsd.org/csrg/share/dict/propernames?revision=61766&view=co'
 
-r = requests.get(url)
-text = r.text
-# print(text)
+names = requests.get(url).text
 
-# We are splitting the words to clear out the empty spaces
-individual_word = text.split()
-# print(individual_word)
+# We are splitting the words to clear out the empty spaces and create iterable
+individual_words = names.split()
 
-# Next we need it to extract a random name we will use random library
-random_number = randint(0, len(individual_word))
-
-# print(individual_word[random_number])
-
-#to get random word with number
-print(individual_word[random_number] + str(random_number))
+# to get random word
+print(random.choice(individual_words))
