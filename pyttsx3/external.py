59b167a7eb2df9e4af80901214644533aabb76ac
lary mak <63947040+larymak@users.noreply.github.com>
2021-07-07 21:18:35 +0300

    Delete pyttsx3 directory

--- pyttsx3/external.py
+++
@@ -1,9 +0,0 @@
-#Using an external event loop
-import pyttsx3
-engine = pyttsx3.init()
-engine.say('The quick brown fox jumped over the lazy dog.', 'fox')
-engine.startLoop(False)
-# engine.iterate() must be called inside externalLoop()
-externalLoop()
-engine.endLoop()
-
--- pyttsx3/volume.py
+++
@@ -1,7 +0,0 @@
-#Changing volume
-import pyttsx3
-engine = pyttsx3.init()
-volume = engine.getProperty('volume')
-engine.setProperty('volume', volume-0.25)
-engine.say('The quick brown fox jumped over the lazy dog.')
-engine.runAndWait()
