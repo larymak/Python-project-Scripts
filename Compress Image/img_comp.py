d7a367d628b2ad7d78056b272d4935fcdbca663a
Xolani Gumbi <xolanigumbi19@gmail.com>
2021-07-08 12:21:37 +0200

    Adde requirements file and fixed a typo on 'img_comp' file.

--- Compress Image/img_comp.py
+++ Compress Image/img_comp.py
@@ -1,7 +1,7 @@
 from PIL import Image
 
 def compress_image(default_image=Image.open('original_image.jpg')):
-	''' Takes an image file and compress it with losing image quality. '''
+	''' Takes an image file and compress it without losing image quality. '''
 
 	''' If no image file is provided, the default image will be compressed '''
 
