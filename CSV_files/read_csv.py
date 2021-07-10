8596d2dfac81b08c9ebe5067a42cd0fc7629cb1d
RaneemAbdulazez <raneem97su@gmail.com>
2021-07-10 17:29:42 +0300

    done

--- CSV_files/read_csv.py
+++ CSV_files/read_csv.py
@@ -1,4 +1,5 @@
 import csv
+import pandas as pd
  
 def read_using_DictReader():
     # opening the CSV file
@@ -10,8 +11,10 @@ def read_using_DictReader():
        for lines in csvFile:
             return lines
 
-
-            
+def read_by_pandas():
+    data=pd.read_csv('CSV_files/assets/addresses.csv')
+    return data.head()       
 
 if __name__=="__main__":
     print(read_using_DictReader())
+    print(read_by_pandas())
