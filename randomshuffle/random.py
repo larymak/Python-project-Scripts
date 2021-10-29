f3509216b0e1eb82be95ea0f3771515f23985871
Anshul Choudhary <34908313+achoudh5@users.noreply.github.com>
2021-10-29 11:01:20 -0400

    Update random.py

--- randomshuffle/random.py
+++ randomshuffle/random.py
@@ -6,15 +6,15 @@ import random
 number_list=input("Enter the array of numbers inside a list: ")
 # Original list
 print(number_list)
-# Output [7, 14, 21, 28, 35, 42, 49, 56, 63, 70]
+# Sample Output [7, 14, 21, 28, 35, 42, 49, 56, 63, 70]
 
 # List after first shuffle
 random.shuffle(number_list)
 print(number_list)
-# Output [42, 28, 14, 21, 7, 35, 49, 63, 56, 70]
+# Sample Output [42, 28, 14, 21, 7, 35, 49, 63, 56, 70]
 
 # List after second shuffle
 random.shuffle(number_list)
 print(number_list)
-# Output [14, 21, 70, 28, 49, 35, 56, 7, 42, 63]
+# Sample Output [14, 21, 70, 28, 49, 35, 56, 7, 42, 63]
 
