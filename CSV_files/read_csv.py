ad9e5db64afbad5a86d99716249cbeb8e5678a6d
Ahmed Hassan <Ahmed.moh.hassan.00@gmail.com>
2022-01-31 14:06:26 +0300

    update on CSV_files
    let the function accept arguments , tuned read_using_DictReader func

--- CSV_files/read_csv.py
+++ CSV_files/read_csv.py
@@ -6,22 +6,27 @@ def read_using_DictReader(path):
     with open(path, mode ='r') as file:   
        # reading the CSV file
        csvFile = csv.DictReader(file)
- 
+       main_dic=[]
        # displaying the contents of the CSV file
        for lines in csvFile:
-            return lines
+           main_dic.append(lines)
+       return main_dic
 
-def read_by_pandas_head():
-    data=pd.read_csv('CSV_files/assets/addresses.csv')
+#retrun the top 5 rows in CSV file
+def read_by_pandas_head(path):
+    data=pd.read_csv(path)
     return data.head()       
 
-def read_by_pandas_tail():
-    data=pd.read_csv('CSV_files/assets/addresses.csv')
+
+#retrun the bottom 5 rows in CSV file
+def read_by_pandas_tail(path):
+    data=pd.read_csv(path)
     return data.tail() 
   
 if __name__=="__main__":
-    print(read_using_DictReader('assets/addresses.csv'))
-    print(read_using_DictReader())
-    print(read_by_pandas_head())
-    print(read_by_pandas_tail())
+    
+    path='./assets/addresses.csv'
+    print(read_using_DictReader(path))
+    print(read_by_pandas_head(path))
+    print(read_by_pandas_tail(path))
 
