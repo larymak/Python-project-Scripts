1ec843d262c13e335d7a62e492fa9fd99931f31e
larymak <larykush@gmail.com>
2022-02-22 09:58:47 +0300

    Python Projects

--- wikipedia/lang.py
+++
@@ -1,6 +0,0 @@
-import wikipedia
-wikipedia.set_lang("fr")
-print(wikipedia.summary("Facebook", sentences=1))
-
-# output : 
-# Facebook est un service de réseautage social en ligne sur Internet permettant d'y publier des informations (photographies, liens, textes, etc.) en contrôlant leur visibilité par différentes catégories de personnes.
