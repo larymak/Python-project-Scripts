0e6050d4163dbb71bfe5063fc4ba0a72a91abd2f
Sifang Shan <shansifang@gmail.com>
2022-03-17 19:53:57 -0700

    Added auto current year in age.py

--- TIME SCRIPTS/Age calculator/age.py
+++ TIME SCRIPTS/Age calculator/age.py
@@ -1,10 +1,13 @@
+from datetime import date
+
+
 name = input("What is your name: ")
 
 Birth_Year = int(input("Enter your birth year: "))
 
-Current_time = 2022
+Current_year = date.today().year
 
 def age_caluu():
-    Age_calculated = (Current_time - Birth_Year)
+    Age_calculated = (Current_year - Birth_Year)
     print ('Hey {} your age is {}'.format(name, Age_calculated))
 age_caluu()
