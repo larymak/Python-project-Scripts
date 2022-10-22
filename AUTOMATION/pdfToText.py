91bd772f8462ee4034471d769ddb67fa66febbc6
Prerna Jagesia <pkjagesia@gmail.com>
2022-10-22 19:51:01 +0530

    Added a test pdf file and made minor changes

--- AUTOMATION/pdfToText.py
+++ AUTOMATION/pdfToText.py
@@ -1,10 +1,15 @@
+import argparse
 import pdfminer.high_level
 
-# Extract with Pdfminer.six Module
-def With_PdfMiner():
-	with open('test.pdf','rb') as fh:
-		doc = pdfminer.high_level.extract_text(fh)
+# Extract text with Pdfminer.six Module
+def With_PdfMiner(pdf):
+	with open(pdf,'rb') as file_handle:
+		doc = pdfminer.high_level.extract_text(file_handle)
 		print(doc)
 
 if __name__ == '__main__':
-	With_PdfMiner()
+	parser = argparse.ArgumentParser()
+	parser.add_argument("file", help = "PDF file from which we extract text")
+	args = parser.parse_args()
+	# print()
+	With_PdfMiner(args.file)
