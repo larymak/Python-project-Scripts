c8d94f448e993c658b3b1df3eb53305b7da0f1ef
Prerna Jagesia <pkjagesia@gmail.com>
2022-10-22 21:25:32 +0530

    Final version of script

--- AUTOMATION/pdfToText.py
+++ AUTOMATION/pdfToText.py
@@ -3,13 +3,15 @@ import pdfminer.high_level
 
 # Extract text with Pdfminer.six Module
 def With_PdfMiner(pdf):
-	with open(pdf,'rb') as file_handle:
-		doc = pdfminer.high_level.extract_text(file_handle)
-		print(doc)
+	with open(pdf,'rb') as file_handle_1:
+		doc = pdfminer.high_level.extract_text(file_handle_1)
+
+	with open('converted_pdf.txt','w') as file_handle_2 :
+		file_handle_2.write(doc)
+
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
 	parser.add_argument("file", help = "PDF file from which we extract text")
 	args = parser.parse_args()
-	# print()
 	With_PdfMiner(args.file)
