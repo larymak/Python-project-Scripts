299bd1b4674979715b999936cdf5001264ce100f
Sonia-Sandler <121911115+Sonia-Sandler@users.noreply.github.com>
2023-01-23 18:37:48 -0500

    Update len_nth_word_from_end.py

--- Data Structures and Algorithms/len_nth_word_from_end.py
+++ Data Structures and Algorithms/len_nth_word_from_end.py
@@ -1,34 +1,5 @@
-# checks the length of the nth word from the end
-def len_nth_word_from_end_1(str, num):
-  plc = -1
-
-  while num != 0 and plc >= -1 * len(str):
-    # accounts for the case of no space at the end of the string after the last word
-    if str[plc] != ' ':
-      while plc >= -1 * len(str) and str[plc] != ' ':
-        plc -= 1
-      else:
-        start = plc +1
-    # accounts for the case of at least one space at the end of the string after the last word
-    else:
-      while plc >= -1 * len(str) and str[plc] == ' ':
-        plc -= 1
-      else:
-        end = plc
-      while plc >= -1 * len(str) and str[plc] != ' ':
-        plc -= 1
-      else:
-        start = plc +1
-    plc = start - 1
-    num -= 1
-    
-  else:
-    if num != 0:
-      return -1
-    return len(str[start: end + 1])
-
 # checks the length of the nth word from the end using split
-def len_nth_word_from_end_2(str, num):
+def len_nth_word_from_end(str, num):
   ls = str.split(" ")
   plc = -1
   word_num = 0
@@ -44,6 +15,5 @@ def len_nth_word_from_end_2(str, num):
     return -1
 
 s = "fly  me  to    the moon "
-
-print(len_nth_word_from_end_1(s, 5))
-print(len_nth_word_from_end_2(s, 3))
+print(len_nth_word_from_end(s, 3))
+print(len_nth_word_from_end(s, 6))
