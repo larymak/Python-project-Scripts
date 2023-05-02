69f8e517459716bc5e2abb0449a2823232b1d1dd
Gideon Clottey <gideonclotteyint@gmail.com>
2023-05-02 23:24:59 +0100

    pulling out the need tags to be pushed to csv

--- WEB SCRAPING/WebScraping-Data-Analytics/pydataanalytics.py
+++ WEB SCRAPING/WebScraping-Data-Analytics/pydataanalytics.py
@@ -1,13 +1,23 @@
 import requests 
 from bs4 import BeautifulSoup
+
+#Url to the jobsite (using tottal job as an examples)
 url =  'https://www.totaljobs.com/jobs/in-london'
 
 r = requests.get(url)
 
 #print(r)
 
+# parsing the html to beautiful soup
 html_soup= BeautifulSoup(r.content, 'html.parser')
-#print(html_soup.prettify())
+
+# Targeting the jobs container
+job_details = html_soup.find('div', class_='ResultsContainer-sc-1rtv0xy-2')
+
+# Pulling out the needed tags
+job_titles =job_details.find_all(['h2','li','dl'])
+
+
 
 
 
