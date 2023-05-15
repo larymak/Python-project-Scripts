8e89c443199ecbbc3dd417c0fa74d4347c9421f8
nikitasmen <82477309+nikitasmen@users.noreply.github.com>
2023-05-15 19:08:57 +0300

    sudoku solver

---
+++ GAMES/sudoku game.py
@@ -0,0 +1,77 @@
+#sudoku game  
+
+board = [
+    [7,8,0,4,0,0,1,2,0],
+    [6,0,0,0,7,5,0,0,9],
+    [0,0,0,6,0,1,0,7,8],
+    [0,0,7,0,4,0,2,6,0],
+    [0,0,1,0,5,0,9,3,0],
+    [9,0,4,0,6,0,0,0,5],
+    [0,7,0,3,0,0,0,1,2],
+    [1,2,0,0,0,7,4,0,0],
+    [0,4,9,2,0,6,0,0,7]
+]
+
+def print_board(bo):
+    for i in range(len(bo)):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - - - ")
+
+        for j in range(len(bo[0])):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+
+            if j == 8:
+                print(bo[i][j])
+            else:
+                print(str(bo[i][j]) + " ", end="")
+
+
+
+def empty_space(bo):
+    for i in range(len(bo)):
+        for j in range(len(bo[i])):
+            if bo[i][j] == 0 :
+                return i,j
+    return None
+
+def place_digit(bo):
+    find = empty_space(bo)
+    if not find:
+        return True 
+    else :
+        row, col = find 
+    
+    for i in range(1,10):
+        if valid(bo,i,(row,col)):
+            bo[row][col] = i 
+
+            if place_digit(bo):
+                return True 
+            bo[row][col] = 0 
+
+    return False 
+
+def valid(bo,num,pos):
+    for i in range(len(bo[0])):
+        if bo[pos[0]][i] == num and pos[1] != i:
+            return False
+
+    for i in range(len(bo)):
+        if bo[i][pos[1]] == num and pos[0] != i:
+            return False
+
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
+    for i in range(box_y*3, box_y*3 + 3):
+        for j in range(box_x * 3, box_x*3 + 3):
+            if bo[i][j] == num and (i,j) != pos:
+                return False
+
+    return True
+
+print_board(board)
+place_digit(board)
+print ("\n\n")
+print_board(board)
