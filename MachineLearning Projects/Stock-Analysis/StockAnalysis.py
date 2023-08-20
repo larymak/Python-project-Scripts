55cd702683abfa4327163f1ebefe1bbd1e839514
M-Basim78 <mbasim.naveed@uetpeshawar.edu.pk>
2023-08-20 21:10:51 +0500

    error removed by changing the function and restructuring the code

--- MachineLearning Projects/Stock-Analysis/StockAnalysis.py
+++ MachineLearning Projects/Stock-Analysis/StockAnalysis.py
@@ -1,64 +1,53 @@
 import yfinance as yf
 import pandas as pd
-import datetime
-from datetime import date
-from pandas_datareader import data as pdr
-import pandas as pd
+from datetime import datetime, timedelta
 import matplotlib.pyplot as plt
 
-stocknum=10
-option=1
-tickerlist = []
-while True:
-  try:
-    option = int(input("You have 2 options 1: Do you want to analyse top stocks in S&P index 2: Specific Stocks, 1/2 ?"))
+def get_ticker_list():
+    while True:
+        try:
+            option = int(input("You have 2 options:\n1. Analyze top stocks in S&P index\n2. Specific Stocks\nEnter option (1/2): "))
+
+            if option == 1:
+                stocknum = int(input("How many stocks do you want to analyze? Enter a number: "))
+                toptickers = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')[0]
+                return toptickers.head(stocknum).Symbol.to_list()
+            elif option == 2:
+                inputstr = input("Enter comma-separated ticker symbols of interested stocks: ")
+                return inputstr.upper().split(",")
+            else:
+                print("Please enter a valid option (1/2).")
+        except ValueError:
+            print("Please input a valid integer only.")
+
+def main():
+    tickerlist = get_ticker_list()
     
-    if option==1:
-        stocknum=int(input("How many stocks you want to analyse? give a number"))
-        toptickers = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')[0]
-        tickerlist= toptickers.head(stocknum).Symbol.to_list()
-        print(tickerlist)
-    elif option==2:
-        inputstr = input("Enter comma separated ticker symbols of interested stocks ")
-        print("Input string: ", inputstr)
+    end_date = datetime.today().strftime('%Y-%m-%d')
+    start_date = (datetime.today() - timedelta(days=365)).strftime('%Y-%m-%d')
+    
+    data = pd.DataFrame()
 
-        # conver to the list
-        tickerlist = inputstr.upper().split (",")
-        print("list: ", list)
+    for ticker in tickerlist:
+        try:
+            y = yf.Ticker(ticker)
+            df = y.history(period="1d", start=start_date, end=end_date)
+            if not df.empty:
+                data[ticker] = df["Close"]
+            else:
+                print("No data found for", ticker)
+        except Exception as e:
+            print("An error occurred while fetching data for", ticker, ":", str(e))
+
+    if not data.empty:
+        data.plot(title="Stock Price Analysis")
+        plt.xlabel("Date")
+        plt.ylabel("Price")
+        plt.show()
+        
+        print(data.head())
     else:
-        raise ValueError
-    break
-  except ValueError:
-      print("Please input valid integer only...")  
-      continue
+        print("No data available for analysis.")
 
-data = pd.DataFrame(columns=tickerlist)
-
-for ticker in tickerlist:
-    y = yf.Ticker(ticker)
-    if(y==0):
-        print("No data found for ",ticker)
-        break
-try:    
-    today=date.today()
-    yesterday=today + datetime.timedelta(days=-1)
-    lastmonth=today + datetime.timedelta(days=-30)
-    lastyear=today + datetime.timedelta(days=-365)
-    df2=yf.download(tickerlist,today,today+datetime.timedelta(days=1), auto_adjust=True)['Close']
-    df3=yf.download(tickerlist,yesterday,yesterday+datetime.timedelta(days=1), auto_adjust=True)['Close']
-    df4=yf.download(tickerlist,lastmonth,lastmonth+datetime.timedelta(days=1), auto_adjust=True)['Close']
-    df5=yf.download(tickerlist,lastyear,lastyear+datetime.timedelta(days=1), auto_adjust=True)['Close']
-    data=data.append(df2)
-    data=data.append(df3)
-    data=data.append(df4)
-    data=data.append(df5)
-    data.plot()
-
-    print(data.head())
-except:
-      print("Error occured while processing. Please try again.") 
-
-
-
-  
-
+if __name__ == "__main__":
+    main()
