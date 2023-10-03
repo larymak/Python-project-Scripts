b139e6e33d5aded3c5c105b47b5447adbb36ab81
Ganesh Kumar <rjeekay24@gmail.com>
2023-10-03 14:13:13 +0530

    Weather app using Django

--- DJANGO PROJECTS/weather-app/MyProject/settings.py
+++ DJANGO PROJECTS/weather-app/MyProject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v#-r-ivf@ph^y!@zr&64jc8tsh#=xmh%fb1x7i-7n(%x)fxa#w'
+SECRET_KEY = 'YOUR_API_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
