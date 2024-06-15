17f4175900bc8f2a1d8c78bbb5ba07e1a376483d
ibrahim-kiri <ibrahimkiri@gmail.com>
2024-06-15 10:49:07 +0300

    Added a student management system

---
+++ SYSTEM MANAGEMENT APPS/Student_management_system/students/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from .models import Student
+
+class StudentForm(forms.ModelForm):
+    class Meta:
+        model = Student
+        fields = ['student_number', 'first_name', 'last_name', 'email', 'field_of_study', 'gpa']
+        labels = {
+            'student_number': 'Student Number',
+            'first_name': 'First Name',
+            'last_name': 'Last Name',
+            'email': 'Email',
+            'field_of_study': 'Field of Study',
+            'gpa': 'GPA'
+        }
+        widgets = {
+            'student_number': forms.NumberInput(attrs={'class': 'form-control'}),
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'field_of_study': forms.TextInput(attrs={'class': 'form-control'}),
+            'gpa': forms.NumberInput(attrs={'class': 'form-control'}),
+        }
