f81809d8ee6e705e693f9b6fac2f53cc5b868eec
shriranga <sbagalkoti@skycliffit.com>
2024-08-06 18:11:42 +0530

    Updated the README.md and the script to include more details

--- INVESTMENT_RULES/README.md
+++ INVESTMENT_RULES/README.md
@@ -1,4 +1,6 @@
-## What Is the Rule of 72? 
+# List of python scripts that auomates calculation of really handful of Investment Rules.
+
+## 1. What Is the Rule of 72? 
     The Rule of 72 is a simple way to determine how long an investment will take to double given a fixed annual rate of interest. Dividing 72 by the annual rate of return gives investors a rough estimate of how many years it will take for the initial investment to duplicate itself. 
 
 ### Key takeaways 
--- INVESTMENT_RULES/rule_of_72.py
+++ INVESTMENT_RULES/rule_of_72.py
@@ -3,9 +3,9 @@ fixed_interest_rate = input("Please enter the Annual Fixed interest rate (don't 
 
 
 def calculate_time_taken_to_double(fixed_interest_rate):
-    # It will get the time zone of the user location
+    # A simple formula calulate the time it takes to double an investment.
     time_taken = 72/float(fixed_interest_rate)
     return time_taken
 
 time_taken_to_double = round(calculate_time_taken_to_double(fixed_interest_rate),2)
-print(f"Your investment will take {time_taken_to_double} year(s) to double")
+print(f"Your investment will take {time_taken_to_double} year(s) to double!")
